9911c13e21fcadaac23e9d062c8a06683418e47e
fajarilf <fjr.ilf.firmansyah@gmail.com>
2023-08-03 21:27:17 +0700

    tugas inosoft 1

---
+++ resources/views/book.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content = "width = device-width, initial-scale=1.0">
+        <title>GATEL</title>
+        <link rel = "stylesheet" href = "{{asset('css/style.css')}}">
+        <script src="https://kit.fontawesome.com/34fcd2309b.js" crossorigin="anonymous"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    </head>
+
+    <body>
+        <ul class = "topnav">
+            <img src="image/dewo.jpg" alt="image">
+            <li class = 'nama'><strong>DEWO ROBOTEAM</strong></li>
+            <form class = "inline">
+                <div class = "input-icons">
+                    <i class = "fas fa-search"></i>
+                    <input  class = "input-field" type="text" name="search" placeholder="Search..">
+                </div>
+            </form>
+            <div  class = "menu-atas">
+                <li><a href = "#home">home</a></li>
+                <li><a href="#news">news</a></li>
+                <li><a href="#contact">contact</a></li>
+                <li><a href="#about">about</a></li>
+            </div>
+            <button class = "button"><span><strong>LOGIN</strong></span></button>
+        </ul>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    </body>
+</html>
